8ffad0bc0db2a9a0fdc1d3922f8ee3d4ab49f038
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-12-31 15:38:44 -0700

    adding show article controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles
-  get 'signup', to: 'users#new'
-  resources :users, except: [:new]
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-  resources :categories, except: [:destroy]
+  # allows us to access the articles
+  # all the routes for articles
+  resources :articles, only: [:show]
 end
