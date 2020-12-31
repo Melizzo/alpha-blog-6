bcbaec7a0e9ae29899cd98c4a686378143649407
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-12-31 14:23:14 -0700

    adding validations to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,5 @@
 class Article < ApplicationRecord
-  belongs_to :user
-  has_many :article_categories
-  has_many :categories, through: :article_categories
-  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  # ensures that an empty article isn't added to the db
+  validates:title, presence: true, length: {minimum: 6, maximum: 100 } 
+  validates:description, presence: true, length: { minimum: 10, maximum: 300 }
 end
